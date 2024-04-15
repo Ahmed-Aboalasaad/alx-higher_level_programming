9350e3101f6399f7a6c3d29a7d3e6fdacd3947a1
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2024-04-15 22:52:18 +0200

    Hot Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
     count = len(sys.argv) - 1
     print(f'{count} argument', end='')
     if count == 0:
-        print('.')
+        print('s.')
     else:
         if count != 1:
             print('s', end='')
         print(':')
         for i, arg in enumerate(sys.argv[1:]):
-            print(f'{i}: {arg}')
+            print(f'{i + 1}: {arg}')
