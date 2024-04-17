24a45c6843961895dba44b7ec5bd250eb1ae67dc
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2024-04-17 06:52:26 +0200

    Hot Fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # Get the defined names
     import hidden_4
     names = dir(hidden_4)
-    
+
     # remove the names that start with __
     for name in names:
         if name[:2] != '__':
