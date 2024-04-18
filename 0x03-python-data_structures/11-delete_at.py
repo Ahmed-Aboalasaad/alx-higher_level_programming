b98e77a6c930d052f164dec288507345f0abf7df
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2024-04-18 10:46:17 +0200

    Fixed Lists & Tuples Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
 def delete_at(my_list=[], idx=0):
-    '''deletes the item at a specific position in a list.'''
+    'deletes the item at a specific position in a list.'
     if (idx < 0 or idx > len(my_list) - 1):
         return my_list
-    newList = []
-    for i in range(0, len(my_list)):
-        if i != idx:
-            newList.append(my_list[i])
-    my_list = newList
-    return newList
+
+    del my_list[idx]
+    return my_list
