d4167a1e15ef31e80f5362c3b6bb789f70bcd296
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2024-07-31 03:24:36 +0300

    JS - Web Scrapping: Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// Prints the content of the file whose path is passed as an argument
+
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.resolve(process.argv[2]);
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(data);
+})
